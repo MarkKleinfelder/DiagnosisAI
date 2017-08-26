2104172819b6dda18ad0e5e4162f3285e524ffc9
Mark <MarkKleinfelder@gmail.com>
2017-08-26 09:26:53 -0600

    successfully added Materialize.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterializeModule } from 'angular2-materialize';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { QAComponent } from './q-a/q-a.component';
+import { ChatComponent } from './chat/chat.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    QAComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/q-a/q-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-q-a',
+  templateUrl: './q-a.component.html',
+  styleUrls: ['./q-a.component.css']
+})
+export class QAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
