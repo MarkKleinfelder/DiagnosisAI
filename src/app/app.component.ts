8827b3709752befc3042a1e9c98af65d052952f4
Mark <MarkKleinfelder@gmail.com>
2017-08-27 16:34:30 -0600

    side nav not working. still.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'materialize-css';
+import 'angular2-materialize';
+import { MaterializeModule, MaterializeDirective } from "angular2-materialize";
+import * as $ from 'jquery';
+import {AfterViewInit} from '@angular/core';    
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'materialize-css';
+import 'angular2-materialize'
+
+import { AppModule } from '../app/app.module';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+   ngOnInit():void {
+      // $(".button-collapse").sideNav(); 
+       }
+
 }
+
+
+   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterializeModule } from 'angular2-materialize';
+import 'materialize-css'
+import { MaterializeModule, MaterializeDirective } from 'angular2-materialize';
 import { RouterModule } from '@angular/router'
 import { FormsModule } from '@angular/forms'
 
