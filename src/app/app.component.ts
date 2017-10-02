e36e01bee10961260b3f798ee2b91da9366c2064
Mark <MarkKleinfelder@gmail.com>
2017-10-02 13:46:52 -0600

    fixing mobile buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import * as $ from 'jquery';
 import {AfterViewInit} from '@angular/core';    
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from '../app/app.module';
 import { environment } from '../environments/environment';
 
