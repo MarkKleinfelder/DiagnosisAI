080191af50d287910a132252cf535fa98ea8b73d
Mark <MarkKleinfelder@gmail.com>
2017-10-03 18:41:08 -0600

    refixing buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 import 'materialize-css';
 import 'angular2-materialize';
 import { MaterializeModule, MaterializeDirective } from "angular2-materialize";
-import * as $ from 'jquery';
+
 import {AfterViewInit} from '@angular/core';    
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from '../app/app.module';
 import { environment } from '../environments/environment';
-declare var require: any;
-var Materialize = require('materialize-css');
+
 
 
 @Component({
